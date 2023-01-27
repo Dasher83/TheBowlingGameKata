097c22c46b50283b3c3f3ed171f6b64460c0d963
Agustín Fernández Chávez <agustinfernandezchavezc@gmail.com>
2023-01-27 12:45:13 -0300

    Separate validation concerns

--- Assets/Scripts/MainFormValidator.cs
+++ Assets/Scripts/MainFormValidator.cs
@@ -8,19 +8,17 @@ public class MainFormValidator : MonoBehaviour
     [SerializeField] 
     private Button _submitButton;
     
-    [SerializeField] 
-    private TMP_Text _errorLabelShot1;
-    
-    [SerializeField] 
-    private TMP_Text _errorLabelShot2;
+    [SerializeField] private TMP_Text _errorLabelShot1;
+    [SerializeField] private TMP_Text _errorLabelShot2;
 
     [SerializeField] private TMP_InputField _shot1Input;
-    
     [SerializeField] private TMP_InputField _shot2Input;
 
     private int _shot1 = -1;
-    
     private int _shot2 = -1;
+
+    private bool _shot1IsValid = false;
+    private bool _shot2IsValid = false;
 
     private const float TimeBetweenValidations = 0.2f;
 
@@ -43,11 +41,39 @@ public class MainFormValidator : MonoBehaviour
         return true;
     }
 
-    public void ValidateRegularShot1 ()
+    private void ValidateRegularShot1 ()
     {
-        bool isValid = ValidateInput(_shot1Input.text, out _shot1);
+        if (string.IsNullOrEmpty(_shot1Input.text)) return;
+        _shot1IsValid = ValidateInput(_shot1Input.text, out _shot1);
 
-        if (!isValid)
+        if (!_shot1IsValid)
+        {
+            _errorLabelShot1.text = "The input must be an integer between 10 and 0.";
+            return;
+        }
+        _errorLabelShot1.text = "";
+    }
+
+    private void ValidateRegularShot2 ()
+    {
+        if (string.IsNullOrEmpty(_shot2Input.text)) return;
+        _shot2IsValid = ValidateInput(_shot2Input.text, out _shot2);
+
+        if (!_shot2IsValid)
+        {
+            _errorLabelShot2.text = "The input must be an integer between 10 and 0.";
+            _submitButton.interactable = false;
+            return;
+        }
+        _errorLabelShot2.text = "";
+    }
+
+    private void ValidateBonusShot1()
+    {
+        if (string.IsNullOrEmpty(_shot1Input.text)) return;
+        _shot1IsValid = ValidateInput(_shot1Input.text, out _shot1);
+
+        if (!_shot1IsValid)
         {
             _errorLabelShot1.text = "The input must be an integer between 10 and 0.";
             _submitButton.interactable = false;
@@ -55,128 +81,72 @@ public class MainFormValidator : MonoBehaviour
         }
 
         _errorLabelShot1.text = "";
+    }
 
-        if (_shot2 < 0)
+    private void ValidateBonusShot2()
+    {
+        if (string.IsNullOrEmpty(_shot2Input.text)) return;
+        _shot2IsValid = ValidateInput(_shot2Input.text, out _shot2);
+
+        if (!_shot2IsValid)
         {
+            _errorLabelShot2.text = "The input must be an integer between 10 and 0.";
             _submitButton.interactable = false;
             return;
         }
-        
-        if (_shot1 + _shot2 > 10)
+
+        _errorLabelShot2.text = "";
+    }
+
+    private void ValidateSumOfShots()
+    {
+        if (_shot1IsValid && _shot2IsValid && _shot1 + _shot2 > 10)
         {
+            _shot1IsValid = false;
+            _shot2IsValid = false;
             _errorLabelShot1.text = "The sum of both shots must be 10 or less.";
-            _submitButton.interactable = false;
-            return;
-        }
-        
-        _submitButton.interactable = true;
-    }
-
-    public void ValidateRegularShot2 ()
-    {
-        bool isValid = ValidateInput(_shot2Input.text, out _shot2);
-
-        if (!isValid)
-        {
-            _errorLabelShot2.text = "The input must be an integer between 10 and 0.";
-            _submitButton.interactable = false;
-            return;
-        }
-
-        _errorLabelShot2.text = "";
-
-        if (_shot1 < 0)
-        {
-            _submitButton.interactable = false;
-            return;
-        }
-
-        if (_shot1 + _shot2 > 10)
-        {
             _errorLabelShot2.text = "The sum of both shots must be 10 or less.";
-            _submitButton.interactable = false;
-            return;
         }
-
-        _submitButton.interactable = true;
     }
 
-    public void ValidateBonusShot1()
+    private void SubmitButtonToggle()
     {
-        bool isValid = ValidateInput(_shot1Input.text, out _shot1);
-
-        if (!isValid)
-        {
-            _errorLabelShot1.text = "The input must be an integer between 10 and 0.";
-            _submitButton.interactable = false;
-            return;
-        }
-
-        if (_shot2 < 0)
+        if(!_shot1IsValid || !_shot2IsValid)
         {
             _submitButton.interactable = false;
-            return;
         }
-
-        _errorLabelShot1.text = "";
-        _submitButton.interactable = true;
-    }
-
-    public void ValidateBonusShot2()
-    {
-        bool isValid = ValidateInput(_shot2Input.text, out _shot2);
-
-        if (!isValid)
+        else
         {
-            _errorLabelShot2.text = "The input must be an integer between 10 and 0.";
-            _submitButton.interactable = false;
-            return;
+            _submitButton.interactable = true;
         }
-
-        if (_shot1 < 0)
-        {
-            _submitButton.interactable = false;
-            return;
-        }
-
-        _errorLabelShot2.text = "";
-        _submitButton.interactable = true;
     }
 
     public void CleanTurn()
     {
         _shot1 = -1; 
         _shot2 = -1;
+        _shot1IsValid = false;
+        _shot2IsValid = false;
     }
 
     private void Update()
     {
         validationTimer -= Time.deltaTime;
-
         if (validationTimer > 0) return;
-        if (!string.IsNullOrEmpty(_shot1Input.text))
+
+        if (_isRegularTurn)
         {
-            if(_isRegularTurn)
-            {
-                ValidateRegularShot1();
-            }
-            else
-            {
-                ValidateBonusShot1();
-            }
+            ValidateRegularShot1();
+            ValidateRegularShot2();
+            ValidateSumOfShots();
         }
-        if (!string.IsNullOrEmpty(_shot2Input.text))
+        else
         {
-            if (_isRegularTurn)
-            {
-                ValidateRegularShot2();
-            }
-            else
-            {
-                ValidateBonusShot2();
-            }
+            ValidateBonusShot1();
+            ValidateBonusShot2();
         }
 
+        SubmitButtonToggle();
         validationTimer = TimeBetweenValidations;
     }
 }
